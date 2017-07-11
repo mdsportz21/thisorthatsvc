b3ec00977eaba9e1449ef722edc199bf9a03e2d3
Timothy Taylor <mdsportz21@gmail.com>
2017-07-10 23:29:21 -0400

    ranking, choosing, saving

---
+++ model/dto.py
@@ -0,0 +1,6 @@
+class SubjectDTO(object):
+    def __init__(self, subjectId = None, imgDesc = None, description = None, imgLink = None):
+        self.subjectId = subjectId
+        self.imgDesc = imgDesc
+        self.description = description
+        self.imgLink = imgLink
---
+++ model/record.py
@@ -0,0 +1,6 @@
+class Subject(object):
+    def __init__(self, _id = None, img_desc = None, description = None, img_link = None):
+        self._id = _id
+        self.img_desc = img_desc
+        self.description = description
+        self.img_link = img_link
